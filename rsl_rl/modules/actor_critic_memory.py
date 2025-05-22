d1874ad81da8d0e73e95cb2231a464745bcf8cfa
Antoine Richard <antoiner@nvidia.com>
2025-05-22 08:41:17 +0200

    push for Ricard

---
+++ rsl_rl/modules/actor_critic_memory.py
@@ -0,0 +1,205 @@
+# Copyright (c) 2021-2025, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import torch
+import torch.nn as nn
+from torch.distributions import Normal
+
+from rsl_rl.utils import resolve_nn_activation
+from rsl_rl.modules.memory_network import HybridMemoryActorNetwork
+
+class ActorCriticMemory(nn.Module):
+    is_recurrent = False
+
+    def __init__(
+        self,
+        num_actor_obs,
+        num_critic_obs,
+        num_actions,
+        actor_hidden_dims=[256, 256, 256],
+        critic_hidden_dims=[256, 256, 256],
+        activation="elu",
+        init_noise_std=1.0,
+        noise_std_type: str = "scalar",
+        clip_actions: bool = False,
+        clip_actions_range: tuple = (-1.0, 1.0),
+        use_embeddings=True,
+        embeddings_size=64,
+        generator_size=(256,256,256),
+        num_memory_obs=64,
+        **kwargs,
+    ):
+        if kwargs:
+            print(
+                "ActorCritic.__init__ got unexpected arguments, which will be ignored: "
+                + str([key for key in kwargs.keys()])
+            )
+        super().__init__()
+        activation = resolve_nn_activation(activation)
+
+        mlp_input_dim_a = num_actor_obs
+        mlp_input_dim_c = num_critic_obs
+        # Policy
+        self.actor = HybridMemoryActorNetwork(
+            num_actor_obs,
+            num_memory_obs,
+            num_actions,
+            actor_hidden_dims=actor_hidden_dims,
+            use_embeddings=use_embeddings,
+            embeddings_size=embeddings_size,
+            generator_size=generator_size,
+            activation=activation,
+            device="cuda",
+            dtype=torch.float32,
+        )
+
+        self.clip_actions = clip_actions
+        self.clip_actions_range = clip_actions_range
+        if self.clip_actions:
+            self.clipping_layer = nn.Tanh()
+    
+        # Value function
+        critic_layers = []
+        critic_layers.append(nn.Linear(mlp_input_dim_c, critic_hidden_dims[0]))
+        critic_layers.append(activation)
+        for layer_index in range(len(critic_hidden_dims)):
+            if layer_index == len(critic_hidden_dims) - 1:
+                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], 1))
+            else:
+                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], critic_hidden_dims[layer_index + 1]))
+                critic_layers.append(activation)
+        self.critic = nn.Sequential(*critic_layers)
+
+        print(f"Actor MLP: {self.actor}")
+        print(f"Critic MLP: {self.critic}")
+
+        # Action noise
+        self.noise_std_type = noise_std_type
+        if self.noise_std_type == "scalar":
+            self.std = nn.Parameter(init_noise_std * torch.ones(num_actions))
+        elif self.noise_std_type == "log":
+            self.log_std = nn.Parameter(torch.log(init_noise_std * torch.ones(num_actions)))
+        else:
+            raise ValueError(f"Unknown standard deviation type: {self.noise_std_type}. Should be 'scalar' or 'log'")
+
+        # Action distribution (populated in update_distribution)
+        self.distribution = None
+        # disable args validation for speedup
+        Normal.set_default_validate_args(False)
+
+    @staticmethod
+    # not used at the moment
+    def init_weights(sequential, scales):
+        [
+            torch.nn.init.orthogonal_(module.weight, gain=scales[idx])
+            for idx, module in enumerate(mod for mod in sequential if isinstance(mod, nn.Linear))
+        ]
+
+    def reset(self, dones=None):
+        pass
+
+    def forward(self):
+        raise NotImplementedError
+
+    @property
+    def action_mean(self):
+        mode = self.distribution.mean
+        if self.clip_actions:
+            mode = ((mode + 1) /2.0)* (self.clip_actions_range[1] - self.clip_actions_range[0]) + self.clip_actions_range[0]
+        return mode
+    
+    @property
+    def action_std(self):
+        return self.distribution.stddev
+    
+    @property
+    def actions_distribution(self) -> torch.Tensor:
+        # Mean and Std concatenated on an extra dimension
+        return torch.stack([self.distribution.mean, self.distribution.stddev], dim=-1)
+
+    @property
+    def entropy(self):
+        return self.distribution.entropy().sum(dim=-1)
+
+    def build_distribution(self, parameters):
+        # build the distribution
+        return Normal(parameters[..., 0], parameters[..., 1])
+
+    def update_distribution(self, observations):
+        # compute mean
+        mean = self.actor(observations)
+        if self.clip_actions:
+            mean = self.clipping_layer(mean)
+
+        # compute standard deviation
+        if self.noise_std_type == "scalar":
+            std = self.std.expand_as(mean)
+        elif self.noise_std_type == "log":
+            std = torch.exp(self.log_std).expand_as(mean)
+        else:
+            raise ValueError(f"Unknown standard deviation type: {self.noise_std_type}. Should be 'scalar' or 'log'")
+        # create distribution
+        self.distribution = Normal(mean, std)
+
+    def act(self, observations, **kwargs):
+        self.update_distribution(observations)
+        act = self.distribution.sample()
+        if self.clip_actions:
+            # Apply tanh to clip the actions to [-1, 1]
+            act = self.clipping_layer(act)
+            # Rescale the actions to the desired range
+            act = ((act + 1) / 2.0) * (self.clip_actions_range[1] - self.clip_actions_range[0]) + self.clip_actions_range[0]
+        return act
+
+    def get_actions_log_prob(self, actions):
+        # Scale the actions to [-1, 1] before computing the log probability.
+        if self.clip_actions:
+            # The unscaled actions still have the tanh applied to them.
+            unscaled_actions = (actions - self.clip_actions_range[0]) / (self.clip_actions_range[1] - self.clip_actions_range[0]) * 2.0 - 1.0
+            # Revert the tanh to get the original actions. We use the TanhBijector to avoid numerical issues.
+            gaussian_actions = self.inverse_tanh(unscaled_actions)
+            return (self.distribution.log_prob(gaussian_actions) - torch.log(1 - unscaled_actions*unscaled_actions + 1e-6)).sum(dim=-1)
+        else:
+            return self.distribution.log_prob(actions).sum(dim=-1)
+
+    def act_inference(self, observations):
+        mode= self.actor(observations)
+        if self.clip_actions:
+            # Apply tanh to clip the actions to [-1, 1]
+            mode = self.clipping_layer(mode)
+            # Rescale the actions to the desired range
+            mode = ((mode + 1) / 2.0) * (self.clip_actions_range[1] - self.clip_actions_range[0]) + self.clip_actions_range[0]
+        return mode
+
+    def evaluate(self, critic_observations, **kwargs):
+        value = self.critic(critic_observations)
+        return value
+
+    def load_state_dict(self, state_dict, strict=True):
+        """Load the parameters of the actor-critic model.
+
+        Args:
+            state_dict (dict): State dictionary of the model.
+            strict (bool): Whether to strictly enforce that the keys in state_dict match the keys returned by this
+                           module's state_dict() function.
+
+        Returns:
+            bool: Whether this training resumes a previous training. This flag is used by the `load()` function of
+                  `OnPolicyRunner` to determine how to load further parameters (relevant for, e.g., distillation).
+        """
+
+        super().load_state_dict(state_dict, strict=strict)
+        return True
+    
+    @staticmethod
+    def atanh(x):
+        return 0.5 * (x.log1p() - (-x).log1p())
+    
+    @staticmethod
+    def inverse_tanh(y):
+        eps = torch.finfo(y.dtype).eps
+        return ActorCritic.atanh(y.clamp(min=-1.0 + eps, max=1.0 - eps))
---
+++ rsl_rl/modules/memory_network.py
@@ -0,0 +1,381 @@
+import torch
+import math
+from rsl_rl.utils import resolve_nn_activation
+
+
+class HybridMemoryNetwork:
+    def __init__(self):
+        pass
+
+
+class MemoryShaper(torch.nn.Module):
+    def __init__(
+            self,
+            input_size,
+            target_size,
+            device=None,
+            dtype=None,
+    ) -> None:
+        super().__init__()
+        self._K = torch.nn.Linear(input_size, target_size[0], device=device, dtype=dtype)
+        self._Q = torch.nn.Linear(input_size, target_size[1], device=device, dtype=dtype)
+
+    def forward(self, input_data):
+        key = self._K(input_data).unsqueeze(-1)
+        query = self._Q(input_data).unsqueeze(-1)
+        d_k = query.size(-1)
+        scores = torch.bmm(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+        return scores
+
+class HybridMemoryRetrieverHead(torch.nn.Module):
+    def __init__(
+            self,
+            input_data_size: int = 12,
+            use_embeddings: bool = True,
+            embeddings_size: int = 32,
+            generator_size: tuple = (256,256),
+            target_network_size: tuple = (32,32),
+            generate_bias: bool = True,
+            activation:str = "elu",
+            device=None,
+            dtype=None,
+            **kwargs,
+    ) -> None:
+        super().__init__()
+        if isinstance(activation, str):
+            activation = resolve_nn_activation(activation)
+
+        self._use_embeddings = use_embeddings
+        if use_embeddings:
+            self.embeddings = torch.nn.Linear(input_data_size, embeddings_size, device=device, dtype=dtype)
+            input_data_size = embeddings_size
+        
+        generator = [torch.nn.Linear(input_data_size, generator_size[0], device=device, dtype=dtype)]
+        generator.append(activation)
+        for i in range(len(generator_size) -1):
+            generator.append(torch.nn.Linear(generator_size[i], generator_size[i+1], device=device, dtype=dtype))
+            generator.append(activation)
+        self._generator = torch.nn.Sequential(*generator)
+        
+        self._weight_scaling_head = MemoryShaper(generator_size[-1], target_network_size, device=device, dtype=dtype)
+        self._weight_additive_head = MemoryShaper(generator_size[-1], target_network_size, device=device, dtype=dtype)
+
+        self._generate_bias = generate_bias
+        if generate_bias:
+            bias_size = (target_network_size[-1], 1)
+            self._bias_scaling_head = MemoryShaper(generator_size[-1], bias_size, device=device, dtype=dtype)
+            self._bias_additive_head = MemoryShaper(generator_size[-1], bias_size, device=device, dtype=dtype)
+        else:
+            self._default_bias_scaling = torch.ones((1, generator_size[-1]), device=device, dtype=dtype)
+            self._default_bias_offset = torch.zeros((1, generator_size[-1]), device=device, dtype=dtype)
+
+    def forward(self, input_data):
+        if self._use_embeddings:
+            input_data = self.embeddings(input_data)
+
+        generator_output = self._generator(input_data)
+
+        # Generate weights
+        weight_scaling = self._weight_scaling_head(generator_output)
+        weight_offset = self._weight_additive_head(generator_output)
+        # Generate bias if requested else use default
+        if self._generate_bias:
+            bias_scaling = self._bias_scaling_head(generator_output).squeeze(1)
+            bias_offset = self._bias_additive_head(generator_output).squeeze(1)
+        else:
+            bias_scaling = self._default_bias_scaling.repeat(input_data.shape[0],1)
+            bias_offset = self._default_bias_offset.repeat(input_data.shape[0],1)
+        return weight_scaling, weight_offset, bias_scaling, bias_offset
+
+class PureMemoryRetrieverHead(torch.nn.Module):
+    def __init__(
+            self,
+            input_data_size: int = 12,
+            use_embeddings: bool = True,
+            embeddings_size: int = 32,
+            generator_size: tuple = (256,256),
+            target_network_size: tuple = (32,32),
+            generate_bias: bool = True,
+            activation:str = "elu",
+            device=None,
+            dtype=None,
+            **kwargs,
+    ) -> None:
+        super().__init__()
+        if isinstance(activation, str):
+            activation = resolve_nn_activation(activation)
+
+        self._use_embeddings = use_embeddings
+        if use_embeddings:
+            self.embeddings = torch.nn.Linear(input_data_size, embeddings_size, device=device, dtype=dtype)
+            input_data_size = embeddings_size
+        
+        generator = [torch.nn.Linear(input_data_size, generator_size[0], device=device, dtype=dtype)]
+        generator.append(activation)
+        for i in range(len(generator_size) -1):
+            generator.append(torch.nn.Linear(generator_size[i], generator_size[i+1], device=device, dtype=dtype))
+            generator.append(activation)
+        self._generator = torch.nn.Sequential(*generator)
+        
+        self._weight_head = MemoryShaper(generator_size[-1], target_network_size, device=device, dtype=dtype)
+
+        self._generate_bias = generate_bias
+        if generate_bias:
+            bias_size = (target_network_size[-1], 1)
+            self._bias_head = MemoryShaper(generator_size[-1], bias_size, device=device, dtype=dtype)
+        else:
+            self._default_bias = torch.zeros((1, generator_size[-1], 1), device=device, dtype=dtype)
+
+    def forward(self, input_data):
+        if self._use_embeddings:
+            input_data = self.embeddings(input_data)
+
+        generator_output = self._generator(input_data)
+
+        # Generate weights
+        weight = self._weight_head(generator_output)
+        # Generate bias if requested else use default
+        if self._generate_bias:
+            bias = self._bias_head(generator_output).squeeze(1)
+        else:
+            bias = self._default_bias.repeat(input_data.shape[0],1)
+        return weight, bias
+
+class HybridLinearModule(torch.nn.Linear):
+    def __init__(self, in_features: int,
+                 out_features: int,
+                 extra_features: int,
+                 use_embeddings: bool = True,
+                 embeddings_size: int = 32,
+                 generator_size: tuple = (256, 256),
+                 bias: bool = True,
+                 activation: str = "elu",
+                 device=None,
+                 dtype=None,
+
+        ) -> None:
+        super().__init__(in_features, out_features, bias=bias, device=device, dtype=dtype)
+
+        self.HMRH = HybridMemoryRetrieverHead(
+            input_data_size=extra_features,
+            use_embeddings=use_embeddings,
+            embeddings_size=embeddings_size,
+            generator_size=generator_size,
+            target_network_size=(in_features, out_features),
+            generate_bias=bias,
+            activation=activation,
+            device=device,
+            dtype=dtype,
+        )
+            
+    def forward(self, in_features, extra_features):
+        # Explode the tuple of weights
+        weight_scaling, weight_offset, bias_scaling, bias_offset = self.HMRH(extra_features)
+        # Compute the affine transformation of the weights
+        weight = self.weight * weight_scaling + weight_offset
+        bias = self.bias * bias_scaling + bias_offset
+        # Compute the Linear transformation
+        return torch.bmm(weight, in_features.unsqueeze(-1)).squeeze() + bias
+
+class PureLinearModule(torch.nn.Module):
+    def __init__(self, in_features: int,
+                 out_features: int,
+                 extra_features: int,
+                 use_embeddings: bool = True,
+                 embeddings_size: int = 32,
+                 generator_size: tuple = (256, 256),
+                 bias: bool = True,
+                 activation: str = "elu",
+                 device=None,
+                 dtype=None,
+        ) -> None:
+        super().__init__()
+
+        self.HMRH = PureMemoryRetrieverHead(
+            input_data_size=extra_features,
+            use_embeddings=use_embeddings,
+            embeddings_size=embeddings_size,
+            generator_size=generator_size,
+            target_network_size=(in_features, out_features),
+            generate_bias=bias,
+            activation=activation,
+            device=device,
+            dtype=dtype,
+        )
+            
+    def forward(self, in_features, extra_features):
+        # Explode the tuple of weights
+        weight, bias = self.HMRH(extra_features)
+        # Compute the Linear transformation
+        return torch.bmm(weight, in_features.unsqueeze(-1)).squeeze() + bias
+
+class HybridMemoryActorNetwork(torch.nn.Module):
+    def __init__(
+            self,
+            num_actor_obs: int = 16,
+            num_memory_obs: int = 12,
+            num_actions: int = 4,
+            actor_hidden_dims = [32, 32],
+            use_embeddings: bool = True,
+            embeddings_size: int = 32,
+            generator_size: tuple = (256, 256),
+            activation = "elu",
+            device=None,
+            dtype=None,
+            **kwargs,
+    ):
+        super().__init__()
+        # Check if activation is a string or a callable
+        if isinstance(activation, str):
+            activation = resolve_nn_activation(activation)
+
+        self.actor = []
+        
+        self.actor.append(HybridLinearModule(num_actor_obs,
+                                        actor_hidden_dims[0],
+                                        num_memory_obs,
+                                        use_embeddings=use_embeddings,
+                                        embeddings_size=embeddings_size,
+                                        generator_size=generator_size,
+                                        bias=True,
+                                        activation=activation,
+                                        device=device,
+                                        dtype=dtype))
+        self.actor.append(activation)
+        for i in range(len(actor_hidden_dims)):
+            if i == len(actor_hidden_dims) - 1:
+                self.actor.append(HybridLinearModule(actor_hidden_dims[i],
+                                                num_actions,
+                                                num_memory_obs,
+                                                use_embeddings=use_embeddings,
+                                                embeddings_size=embeddings_size,
+                                                generator_size=generator_size,
+                                                bias=True,
+                                                activation=activation,
+                                                device=device,
+                                                dtype=dtype))
+            else:
+                self.actor.append(HybridLinearModule(actor_hidden_dims[i],
+                                                actor_hidden_dims[i + 1],
+                                                num_memory_obs,
+                                                use_embeddings=use_embeddings,
+                                                embeddings_size=embeddings_size,
+                                                generator_size=generator_size,
+                                                bias=True,
+                                                activation=activation,
+                                                device=device,
+                                                dtype=dtype))
+                self.actor.append(activation)
+
+    def forward(self, x, memory_data):
+        for act in self.actor:
+            if isinstance(act, HybridLinearModule):
+                x = act(x, memory_data)
+            else:
+                x = act(x)
+        return x
+    
+class PureMemoryActorNetwork(torch.nn.Module):
+    def __init__(
+            self,
+            num_actor_obs: int = 16,
+            num_memory_obs: int = 12,
+            num_actions: int = 4,
+            actor_hidden_dims = [32, 32],
+            use_embeddings: bool = True,
+            embeddings_size: int = 32,
+            generator_size: tuple = (256, 256),
+            activation = "elu",
+            device=None,
+            dtype=None,
+            **kwargs,
+    ):
+        super().__init__()
+        # Check if activation is a string or a callable
+        if isinstance(activation, str):
+            activation = resolve_nn_activation(activation)
+
+        self.actor = []
+        
+        self.actor.append(PureLinearModule(num_actor_obs,
+                                        actor_hidden_dims[0],
+                                        num_memory_obs,
+                                        use_embeddings=use_embeddings,
+                                        embeddings_size=embeddings_size,
+                                        generator_size=generator_size,
+                                        bias=True,
+                                        activation=activation,
+                                        device=device,
+                                        dtype=dtype))
+        self.actor.append(activation)
+        for i in range(len(actor_hidden_dims)):
+            if i == len(actor_hidden_dims) - 1:
+                self.actor.append(PureLinearModule(actor_hidden_dims[i],
+                                                num_actions,
+                                                num_memory_obs,
+                                                use_embeddings=use_embeddings,
+                                                embeddings_size=embeddings_size,
+                                                generator_size=generator_size,
+                                                bias=True,
+                                                activation=activation,
+                                                device=device,
+                                                dtype=dtype))
+            else:
+                self.actor.append(PureLinearModule(actor_hidden_dims[i],
+                                                actor_hidden_dims[i + 1],
+                                                num_memory_obs,
+                                                use_embeddings=use_embeddings,
+                                                embeddings_size=embeddings_size,
+                                                generator_size=generator_size,
+                                                bias=True,
+                                                activation=activation,
+                                                device=device,
+                                                dtype=dtype))
+                self.actor.append(activation)
+
+    def forward(self, x, memory_data):
+        for act in self.actor:
+            if isinstance(act, PureLinearModule):
+                x = act(x, memory_data)
+            else:
+                x = act(x)
+
+        return x
+
+if __name__ == "__main__":
+    import torch
+
+    input_data = torch.randn(1000, 16, device="cuda")
+    memory_data = torch.randn(1000, 12, device="cuda")
+
+    model = HybridMemoryActorNetwork(
+        num_actor_obs=16,
+        num_memory_obs=12,
+        num_actions=4,
+        actor_hidden_dims=[32, 32],
+        use_embeddings=True,
+        embeddings_size=32,
+        generator_size=(256, 256),
+        activation="elu",
+        device="cuda",
+        dtype=torch.float32,
+    )
+    for i in range(100):
+        output = model(input_data, memory_data)
+    print("actions shape:",output.shape)
+
+    model = PureMemoryActorNetwork(
+        num_actor_obs=16,
+        num_memory_obs=12,
+        num_actions=4,
+        actor_hidden_dims=[32, 32],
+        use_embeddings=True,
+        embeddings_size=32,
+        generator_size=(256, 256),
+        activation="elu",
+        device="cuda",
+        dtype=torch.float32,
+    )
+    for i in range(100):
+        output = model(input_data, memory_data)
+    print("actions shape:",output.shape)
